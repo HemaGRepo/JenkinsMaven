863ba6ad8003eea2a574aec2fc65dafdb447c093
Hema G <hema.g@capgemini.com>
2018-03-19 14:55:26 +0530

    c3

--- DemoMar/src/HelloWolrd.java
+++ DemoMar/src/HelloWolrd.java
@@ -2,7 +2,7 @@
 public class HelloWolrd {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to DevOps");
+		System.out.println("Welcome to DevOps...");
 		for(int i=1;i<=20;i++)
 			System.out.println("Welcome to DevOps "+i);
 		
